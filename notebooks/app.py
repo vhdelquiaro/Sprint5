156ef120acc21a2780e2fcda8936770fc48ee841
vhdelquiaro <vitordelquiaro@hotmail.com>
2025-10-20 19:36:09 -0300

    Adicionar configuração Streamlit e finalizar projeto

---
+++ notebooks/app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles.csv') # lendo os dados
+hist_button = st.button('Criar histograma') # criar um botão
+        
+        if hist_button: # se o botão for clicado
+            # escrever uma mensagem
+            st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+            
+            # criar um histograma
+            fig = px.histogram(car_data, x="odometer")
+        
+            # exibir um gráfico Plotly interativo
+            st.plotly_chart(fig, use_container_width=True)
+        
+disp_button = st.button('Criar gráfico de dispersão') # criar um botão
+        
+        if disp_button: # se o botão for clicado
+            # escrever uma mensagem
+            st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+            
+            # criar um gráfico de dispersão
+            fig = px.scatter(car_data, x="odometer", y="price") # criar um gráfico de dispersão
+        
+            # exibir um gráfico Plotly interativo
+            st.plotly_chart(fig, use_container_width=True)
+        
